08e4a010bd81d570082a223bf39a4b1ff16157cb
sethkelley2 <touchkelley@gmail.com>
2023-05-17 14:45:31 -0700

    added validation dataset and validation loss

--- trainer.py
+++ trainer.py
@@ -6,7 +6,7 @@ from utils import LidarDataset,targets_dict
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import matplotlib.pyplot as plt
-
+import pickle
 
 lidar_path = "/media/aerotractai/Archive100A/Wiggins_20230509/Aerotract (shared)/LiDARClassified/BV1_Circuit_Beaver_Creek_1_READY/cloud328819b9aee51228.las"
 dataset = LidarDataset(lidar_path)
@@ -32,6 +32,13 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(),lr = 0.005)
 epochs = 40
 
+#validation dataset
+val_path = "/media/aerotractai/Archive100A/Wiggins_20230509/Aerotract (shared)/LiDARClassified/BV1_Circuit_Beaver_Creek_1_READY/cloud328819ad3d77f188.las"
+val_dataset = LidarDataset(val_path)
+val_dataset.to_device(device)
+val_dataloader = DataLoader(val_dataset,batch_size=1000000)
+
+#loss list
 train_loss_list = []
 val_loss_list = []
 print("starting training loop")
@@ -49,8 +56,30 @@ for epoch in range(epochs):
         optimizer.zero_grad()
 
     print(loss)
-    train_loss_list.append(loss)
+    train_loss_list.append(loss.item())
+
+    with torch.no_grad():
+        for x,y in val_dataloader:
+            x = x.reshape(1,-1,7)
+            y = y.apply_(targets_map).reshape(-1)
+
+            z = model(x)
+            yhat = z.squeeze(0).T
+            loss = criterion(yhat,y)
+
+    val_loss_list.append(loss.item())
+
+    print(train_loss_list,val_loss_list)
+
+with open('train_loss_list.pkl', 'wb') as file1:
+    pickle.dump(train_loss_list, file1)
+
+# Pickle and save list2
+with open('val_loss_list.pkl', 'wb') as file2:
+    pickle.dump(val_loss_list, file2)
 
 fig, ax = plt.subplots()
-ax.plot(train_loss_list)
+ax.plot(train_loss_list, label='Train Loss')
+ax.plot(val_loss_list, label='Validation Loss')
+ax.legend()  # Add a legend to differentiate the lines
 plt.show()
